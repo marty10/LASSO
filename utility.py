0871d4115a402bd828108065bc74a07aa124e94a
marty10 <martychuncie@alice.it>
2015-12-15 00:44:39 +0000

    ciao

--- utility.py
+++ utility.py
@@ -275,7 +275,7 @@ def extract_max_from_beta(dictlist):
     weights_indexes = np.zeros(len(dictlist))
     for j,dict in enumerate(dictlist):
         if len(dict)!=0:
-            dict_values = np.array(dict.values())
+            dict_values = np.array(list(dict.values()))
             dict_keys = 1+np.array(list(dict.keys()))
             key_values = dict_values*dict_keys
             max_value = np.max(np.abs(key_values))
