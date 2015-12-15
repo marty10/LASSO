395f4098f50fb5dba945f02aafa4aa7d86a783ff
marty10 <martychuncie@alice.it>
2015-12-15 13:45:42 +0000

    cio

--- binning_cross_val.py
+++ binning_cross_val.py
@@ -1,11 +1,10 @@
 import sys
-
-__author__ = 'Martina'
 from sklearn import linear_model
 from ExtractDataset import Dataset
 import numpy as np
 from utility import generate_samples, get_current_data, compute_mse, extract_losses, get_common_indexes, \
-    extracte_chosen_indexes, get_common_indexes1, get_common_indexes_binning, extract_max_from_beta
+    extract_chosen_indexes, get_common_indexes1, get_common_indexes_binning, extract_max_from_beta, \
+    extract_chosen_indexes_from_start
 
 n_samples = 200
 n_features = 500
