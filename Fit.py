aab608ffc7995bff7ab1cbc46d23895c10c4f54a
marty10 <martychuncie@alice.it>
2016-02-25 18:05:32 +0100

    nearest_point

--- Fit.py
+++ Fit.py
@@ -4,7 +4,7 @@ from abc import ABCMeta
 from sklearn.linear_model.base import center_data
 from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
-from Kernel import Scalar_kernel
+from Kernel import Scalar_kernel, Gaussian_kernel
 from Transformation import EnelTransformation
 from utility import center_test, compute_lasso
 
@@ -17,6 +17,30 @@ class Fit:
     def fitting(self, X, Y, XTest, YTest):
         """Fit data"""
 
+class Enel_gaussianKernel(Fit):
+    def __init__(self,):
+        pass
+
+    def fitting(self,XTrain, YTrain, XTest,YTest):
+
+        XTrain_transf = XTrain
+        XTest_transf = XTest
+
+        YTest_transf = Gaussian_kernel(sigma = 0.5).transform_y(XTest_transf,YTest)
+        YTrain_transf = Gaussian_kernel(sigma = 0.5).transform_y(XTrain_transf,YTrain)
+
+        XTrain_transf= Gaussian_kernel(sigma = 0.5).transform(XTrain_transf)
+        XTest_transf= Gaussian_kernel(sigma = 0.5).transform(XTest_transf)
+
+
+        ##centratura dei dati
+        XTrain_transf, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain_transf, fit_intercept=True, normalize = True)
+        XTest_transf, YTest_ = center_test(XTest_transf,YTest_transf,X_mean,y_mean,X_std)
+
+        new_loss, _ = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_, score ="r2_score")
+
+        print("loss enel velocita :", new_loss )
+        return XTrain_transf, XTest_transf
 
 class Enel_scalarKernel(Fit):
     def __init__(self,):
@@ -24,27 +48,24 @@ class Enel_scalarKernel(Fit):
 
     def fitting(self,XTrain, YTrain, XTest,YTest):
 
-        transf = EnelTransformation()
-        #XTrain_transf,_ = transf.transform(XTrain)
-        #XTest_transf,_ = transf.transform(XTest)
         XTrain_transf = XTrain
         XTest_transf = XTest
 
         YTest_transf = Scalar_kernel().transform_y(XTest_transf,YTest)
         YTrain_transf = Scalar_kernel().transform_y(XTrain_transf,YTrain)
 
-        XTrain_transf = Scalar_kernel().transform(XTrain_transf)
-        XTest_transf = Scalar_kernel().transform(XTest_transf)
+        XTrain_transf, dict_ = Scalar_kernel().transform(XTrain_transf)
+        XTest_transf, dict_ = Scalar_kernel().transform(XTest_transf)
 
 
         ##centratura dei dati
         XTrain_transf, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain_transf, fit_intercept=True, normalize = True)
         XTest_transf, YTest_ = center_test(XTest_transf,YTest_transf,X_mean,y_mean,X_std)
 
-        new_loss, _ = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_, score ="mean_squared_error")
+        new_loss, _ = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_, score ="r2_score")
 
         print("loss enel velocita :", new_loss )
-        return XTrain_transf, XTest_transf
+        return XTrain_transf, XTest_transf, dict_
 
 class Enel_power3(Fit):
     def __init__(self,):
@@ -58,7 +79,7 @@ class Enel_power3(Fit):
 
         ##centratura dei dati
         XTrain_transf, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain, fit_intercept=True, normalize = True)
-        XTest_transf, YTest_ = center_test(XTest_transf,YTest,X_mean,y_mean,X_std)
+        XTest_transf, YTest_ = center_test(XTest_transf,YTest,X_mean,y_mean,X_std, normalize=True)
 
         new_loss, _ = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_, score ="r2_score")
 
@@ -76,10 +97,10 @@ class Linear_fit(Fit):
         XTest_, YTest_ = center_test(XTest,YTest,X_mean,y_mean,X_std)
 
         ##compute linear lasso
-        new_loss, beta = compute_lasso(XTrain_, YTrain_, XTest_, YTest_)
+        new_loss, beta = compute_lasso(XTrain_, YTrain_, XTest_, YTest_,score = "r2_score")
         print("loss lineare", new_loss)
 
-class Polynomial_fit(Fit):
+class Power_fit(Fit):
     def __init__(self,):
         pass
 
@@ -88,6 +109,27 @@ class Polynomial_fit(Fit):
         ###trasformazione non lineare
         for degree in range(2,5):
             poly = PolynomialFeatures(degree=degree,include_bias=False)
+            XTrain_transf = np.power(XTrain,degree)
+            XTest_transf = np.power(XTest,degree)
+
+            ##centratura dei dati
+            XTrain_transf, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain, fit_intercept=True, normalize = True)
+            XTest_transf, YTest_ = center_test(XTest_transf,YTest,X_mean,y_mean,X_std)
+
+            new_loss, _ =compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_,score = "r2_score")
+
+            print("loss polinomio grado", str(degree),":", new_loss )
+
+
+class Polynomial_fit(Fit):
+    def __init__(self,):
+        pass
+
+    def fitting(self,XTrain, YTrain, XTest,YTest ):
+
+        ###trasformazione non lineare
+        for degree in range(2,3):
+            poly = PolynomialFeatures(degree=degree,include_bias=False)
             XTrain_transf = poly.fit_transform(XTrain)
             XTest_transf = poly.fit_transform(XTest)
 
@@ -95,7 +137,7 @@ class Polynomial_fit(Fit):
             XTrain_transf, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain, fit_intercept=True, normalize = True)
             XTest_transf, YTest_ = center_test(XTest_transf,YTest,X_mean,y_mean,X_std)
 
-            new_loss, _ =compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_)
+            new_loss, _ =compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_,score = "r2_score")
 
             print("loss polinomio grado", str(degree),":", new_loss )
 
@@ -116,7 +158,7 @@ class Log_y_fit(Fit):
         XTrain_, YTrain_, X_mean, y_mean, X_std = center_data(XTrain, YTrain_, fit_intercept=True, normalize = True)
         XTest_, YTest_ = center_test(XTest,YTest_,X_mean,y_mean,X_std)
 
-        new_loss, _ = compute_lasso(XTrain_, YTrain_, XTest_, YTest_)
+        new_loss, _ = compute_lasso(XTrain_, YTrain_, XTest_, YTest_,score = "r2_score")
 
         print("loss log(y) :", new_loss )
 
@@ -139,7 +181,7 @@ class Inverse_y_fit(Fit):
         XTrain_, YTrain_, X_mean, y_mean, X_std = center_data(XTrain, YTrain_, fit_intercept=True, normalize = True)
         XTest_, YTest_ = center_test(XTest,YTest_,X_mean,y_mean,X_std)
 
-        new_loss, _ =compute_lasso(XTrain_, YTrain_, XTest_, YTest_)
+        new_loss, _ =compute_lasso(XTrain_, YTrain_, XTest_, YTest_,score = "r2_score")
 
         print("loss inverse :", new_loss )
 
@@ -161,7 +203,7 @@ class Inverse_x_fit(Fit):
         XTrain_, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain, fit_intercept=True, normalize = True)
         XTest_, YTest_ = center_test(XTest_transf,YTest,X_mean,y_mean,X_std)
 
-        new_loss, _ =compute_lasso(XTrain_, YTrain_, XTest_, YTest_)
+        new_loss, _ =compute_lasso(XTrain_, YTrain_, XTest_, YTest_,score = "r2_score")
 
         print("loss inverse x:", new_loss )
 
@@ -182,7 +224,7 @@ class Sqrt_fit(Fit):
         XTrain_, YTrain_, X_mean, y_mean, X_std = center_data(XTrain, YTrain_, fit_intercept=True, normalize = True)
         XTest_, YTest_ = center_test(XTest,YTest_,X_mean,y_mean,X_std)
 
-        new_loss, _ = compute_lasso(XTrain_, YTrain_, XTest_, YTest_)
+        new_loss, _ = compute_lasso(XTrain_, YTrain_, XTest_, YTest_,score = "r2_score")
 
         print("loss sqrt(y) :", new_loss )
 
@@ -206,7 +248,7 @@ class Log_x_fit(Fit):
         XTrain_transf, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain, fit_intercept=True, normalize = True)
         XTest_transf, YTest_ = center_test(XTest_transf,YTest,X_mean,y_mean,X_std)
 
-        new_loss, beta_ordered_values = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_)
+        new_loss, beta_ordered_values = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_,score = "r2_score")
 
         print("loss log(x) :", new_loss )
 
@@ -238,6 +280,6 @@ class Log_xy_fit(Fit):
         XTrain_transf, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_transf, YTrain_, fit_intercept=True, normalize = True)
         XTest_transf, YTest_ = center_test(XTest_transf,YTest_,X_mean,y_mean,X_std)
 
-        new_loss,_ = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_)
+        new_loss,_ = compute_lasso(XTrain_transf, YTrain_, XTest_transf, YTest_, score = "r2_score")
 
         print("loss log(y) e log(x) :", new_loss )
