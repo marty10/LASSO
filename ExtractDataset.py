e7d24b6534ecf565c6b1a40b25704e641a83641e
marty10 <martychuncie@alice.it>
2016-02-25 17:22:03 +0100

    nearest_point

--- ExtractDataset.py
+++ ExtractDataset.py
@@ -9,7 +9,6 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model.base import center_data
 
 from Transformation import NullTransformation
-from old.FeatureSelectionRules import null_rule
 
 
 class Dataset:
