df8655b185a07601f2b40b488e23d59d6bf84062
marty10 <martychuncie@alice.it>
2016-02-26 18:17:14 +0100

    ranking_product

---
+++ save_dict.py
@@ -0,0 +1,24 @@
+from ExtractResult import Result
+from Transformation import EnelWindSpeedTransformation
+import numpy as np
+
+from utility import find_nearest
+
+folder_train = "ENEL_2014/PSC/0-23_0001-0049/"
+folder_test = "ENEL_2014/PSC/24-47_0001-0049/"
+label_file = "ENEL_2014/PSC/Metering_2011-2014_UTC.txt"
+
+file = "ENEL_2014/Enel_dataset.npz"
+results = Result(file, "lasso")
+
+XTrain, YTrain, XTest, YTest = results.extract_train_test()
+
+Coord = np.load("ENEL_2014/Coord.npz")["Coord"]
+k=2
+neight_= find_nearest(Coord,k)
+
+enel_transf = EnelWindSpeedTransformation()
+XTrain, dict_ = enel_transf.transform(XTrain)
+XTrain, output_dict = enel_transf.nearest_products_levels(neight_,dict_,XTrain)
+
+np.savez("ENEL_2014/Product_level_2_dict", dict_ = output_dict)
