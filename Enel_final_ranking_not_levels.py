ffca3f574fa690a81a9ee74139a9c3cffbb69281
marty10 <martychuncie@alice.it>
2016-03-05 19:45:36 +0100

    weights_all parameter final ranking

--- Enel_final_ranking_not_levels.py
+++ Enel_final_ranking_not_levels.py
@@ -23,8 +23,8 @@ else:
 
 folder = "ENEL_2014/"
 ext = ".npz"
-file_cross_val =  folder+file_name+ext
-fine_name_weights = file_name+"ranking"
+file_cross_val = folder+file_name+ext
+fine_name_weights = file_name+"ranking_not_levels"
 
 results_cross_val = Result(file_cross_val, "lasso")
 results_weighted_lasso = Result(fine_name_weights, "lasso")
