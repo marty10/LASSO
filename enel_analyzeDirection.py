ca65c23a4eda51cc28d39069e1d62d746a302d09
marty10 <martychuncie@alice.it>
2016-03-09 12:13:23 +0100

    correzione

--- enel_analyzeDirection.py
+++ enel_analyzeDirection.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.cross_validation import train_test_split
 
-from Enel_utils import compute_angle, extract_direction, create_dict_direction
+from Enel_utils import compute_angle, extract_direction, create_dict_direction, compute_angle_new
 from ExtractResult import Result
 
 ##load the dataset
@@ -19,6 +19,7 @@ XTrain, YTrain, XTest, YTest = results.extract_train_test()
 
 angular_coeffs = compute_angle(Coord, Coord_turb)
 
+print("fatto")
 enel_transf = Enel_powerCurveTransformation()
 XTrain_angle,dict_ = enel_transf.compute_angle_matrix(XTrain)
 
@@ -29,20 +30,20 @@ output_dict_ = dict.fromkeys(np.arange(0,49),np.array([[]], dtype = "int64"))
 
 XTrain_transf = np.array([[]])
 XTest_transf = np.array([[]])
-#
-# XTrain_transf, output_dict_ = enel_transf.transform1(turbine_dict, enel_dict, XTrain, power_curve,0, XTrain_transf, output_dict_)
-# np.savez("ENEL_2014/turbine_180", turbine_dict = turbine_dict, XTrain_transf = XTrain_transf)
-#
-# XTest_transf, _ = enel_transf.transform1(turbine_dict, enel_dict, XTest, power_curve,0, XTest_transf, output_dict_.copy())
-# np.savez("ENEL_2014/turbine_180", turbine_dict = turbine_dict, XTrain_transf = XTrain_transf, XTest_transf = XTest_transf)
-#
-# ##analyze turbine alone
-# print("loss 1 turibina solo sul validation")
-# XTrain_, XVal_, YTrain_, YVal_ = train_test_split(XTrain_transf, YTrain, test_size=0.33,random_state=0)
-# Linear_fit().fitting(XTrain_, YTrain_, XVal_,YVal_, [])
-#
-# print("loss 1 turibina solo sul test")
-# Linear_fit().fitting(XTrain_transf, YTrain, XTest_transf,YTest,values_TM)
+
+XTrain_transf, output_dict_ = enel_transf.transform1(turbine_dict, enel_dict, XTrain, power_curve,0, XTrain_transf, output_dict_)
+np.savez("ENEL_2014/turbine_180", turbine_dict = turbine_dict, XTrain_transf = XTrain_transf)
+
+XTest_transf, _ = enel_transf.transform1(turbine_dict, enel_dict, XTest, power_curve,0, XTest_transf, output_dict_.copy())
+np.savez("ENEL_2014/turbine_180", turbine_dict = turbine_dict, XTrain_transf = XTrain_transf, XTest_transf = XTest_transf)
+
+##analyze turbine alone
+print("loss 1 turibina solo sul validation")
+XTrain_, XVal_, YTrain_, YVal_ = train_test_split(XTrain_transf, YTrain, test_size=0.33,random_state=0)
+Linear_fit().fitting(XTrain_, YTrain_, XVal_,YVal_, [])
+
+print("loss 1 turibina solo sul test")
+Linear_fit().fitting(XTrain_transf, YTrain, XTest_transf,YTest,values_TM)
 
 
 ##compute mean turbine
