69095b6941cbc86376ee690b33d48d234f232bf7
marty10 <martychuncie@alice.it>
2016-03-14 23:32:52 +0100

    add cast a lista

--- Enel_final_ranking.py
+++ Enel_final_ranking.py
@@ -29,8 +29,9 @@ results_cross_val = Result(file_cross_val, "lasso")
 results_weighted_lasso = Result(fine_name_weights, "lasso")
 
 mses = results_weighted_lasso.extract_mses()
-iter = np.argmin(mses)
-print ("iter chosen:",iter, "with mse:",mses[iter])
+mses_int = list(map(int, mses))
+iter = np.argmin(mses_int)
+print ("iter chosen:",iter, "with mse:",mses_int[iter])
 print("--------------")
 
 indexes_beta = results_weighted_lasso.extract_beta_div_zeros()[iter]
