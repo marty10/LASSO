dcd21e01c31b682121e2224db312c630ff759a93
marty10 <martychuncie@alice.it>
2016-03-15 20:42:03 +0100

    clean piu cross val nuovo

--- Enel_cross_val_blocks_directionPerSampleSingleAndAllLevel.py
+++ Enel_cross_val_blocks_directionPerSampleSingleAndAllLevel.py
@@ -2,44 +2,62 @@ from sklearn import linear_model
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model.base import center_data
 import numpy as np
+from Enel_utils import find_nearest_turbine, find_turbines_nearest_points, compute_angle
 from ExtractResult import Result
-from Transformation import EnelTransformation
-from utility import generate_samples_dynamic_set, get_current_data, compute_mse, get_common_indexes, \
+from Lasso_utils import compute_lasso, compute_mse
+from Transformation import Enel_powerCurveTransformation, EnelWindSpeedTransformation, \
+    Enel_directionPowerCurveTransformation
+from utility import generate_samples_dynamic_set, get_current_data,get_common_indexes, \
     extract_chosen_indexes_from_start, center_test
 import sys
-from Transformation import EnelWindSpeedTransformation
+
+sys.argv[1:] = [str(x) for x in sys.argv[1:]]
+output_folder = sys.argv[1]
 
 ####load data
 file = "ENEL_2014/Enel_dataset.npz"
 results = Result(file, "lasso")
 
-XTrain, YTrain, XTest, YTest = results.extract_train_test()
 
-sys.argv[1:] = [str(x) for x in sys.argv[1:]]
-kind_transformation = sys.argv[1]
+XTrain, YTrain, XTest, YTest = results.extract_train_test()
+enel_dict = results.extract_dict()
+Coord, Coord_turb, power_curve = results.extract_coords()
+
+angles_coord_turb,_ = compute_angle(Coord, Coord_turb)
 
 ##transformation of data
+X = np.concatenate((XTrain, XTest), axis = 0)
+enel_transf = Enel_powerCurveTransformation()
 
-if kind_transformation=="windSpeed":
-    transf = EnelWindSpeedTransformation()
-    YTrain = YTrain**(1./3)
-    YTest = YTest**(1./3)
-    output_file_name = "Enel_cross_val_blocks_wind_speed.npz"
-elif kind_transformation=="windSpeedPower3":
-    transf = EnelTransformation()
-    output_file_name = "Enel_cross_val_blocks.npz"
+X_angle,_,_ = enel_transf.compute_angle_matrix(X)
+output_dict = dict.fromkeys(np.arange(0,49),np.array([[]], dtype = "int64"))
 
-XTrain_transf, dict_ = transf.transform(XTrain)
-XTest_transf, dict_ = transf.transform(XTest)
+k_levels = np.arange(0,12).reshape([12,1])
+for key in np.arange(0,49):
+    current_values = np.arange(key*12,key*12+12).reshape([12,1])
+    output_dict[key] = np.concatenate((current_values,k_levels), axis = 1)
 
-##center data
+X_speed,_ = EnelWindSpeedTransformation().transform(X)
+print("wind speed computed")
+
+enel_transf = Enel_directionPowerCurveTransformation()
+X_transf, dict_sample_turb = enel_transf.transform(X_angle, angles_coord_turb, X_speed, power_curve, Coord, Coord_turb)
+print("single transformation done")
+
+X_transf, output_dict = enel_transf.transformPerTurbineLevel(dict_sample_turb, enel_dict, X, power_curve, X_transf,output_dict)
+print("transformation per turbine done")
+
+XTrain_transf = X_transf[:XTrain.shape[0],:]
+XTest_transf = X_transf[XTrain.shape[0]:,:]
+
 ##center data
 XTrain_noCenter, XVal_noCenter, YTrain_noCenter, YVal_noCenter = train_test_split(XTrain_transf, YTrain, test_size=0.33,random_state=0)
 XTrain_, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_noCenter, YTrain_noCenter, fit_intercept=True, normalize = True)
 XVal_, YVal_ = center_test(XVal_noCenter,YVal_noCenter,X_mean,y_mean,X_std)
 
-#new_loss, _ = compute_lasso(XTrain_, YTrain_, XVal_, YVal_,score = "mean_squared_error")
-#print("loss", new_loss)
+values_TM = []
+new_loss, _ = compute_lasso(XTrain_, YTrain_, XVal_, YVal_,score = "mean_squared_error", values_TM = np.array([[24,281], [24,214]]))
+print("loss", new_loss)
 
 n_features_transf = XTrain_.shape[1]
 
@@ -70,13 +88,14 @@ num_cycle = 0
 cycles = 10
 min_set = 5
 max_set = min_set+5
-max_active_set = int(n_features_transf/10)
+max_active_set = 49
 active_set = 0
 
 compute_mse_current = 0
 lasso_cv = linear_model.LassoCV(fit_intercept=False, n_jobs = -1)
 flag_linear = 0
 score = "mean_squared_error"
+
 
 while num_cycle<cycles:
 
@@ -107,7 +126,7 @@ while num_cycle<cycles:
         rand_vect = r1.choice(n_samples_val,active_set_samples, replace = False)
         x_val_i = x_val_i[rand_vect,:]
         YVal_i = YVal_[rand_vect]
-        new_loss, beta= compute_mse(model, x_train_i, YTrain_,x_val_i, YVal_i, score)
+        new_loss, beta= compute_mse(model, x_train_i, YTrain_,x_val_i, YVal_i, score, values_TM)
         losses.append(new_loss)
         betas.append(beta)
 
@@ -143,10 +162,11 @@ while num_cycle<cycles:
     assert(len(saved_indexes)<=max_active_set)
     x_train_saved, x_val_saved = get_current_data(XTrain_, XVal_, saved_indexes)
 
+
     if compute_mse_current:
         x_val_saved = x_val_saved[rand_vect,:]
         YVal_saved = YVal_[rand_vect]
-        mse_saved,_= compute_mse(model, x_train_saved, YTrain_,x_val_saved, YVal_saved, score)
+        mse_saved,_= compute_mse(model, x_train_saved, YTrain_,x_val_saved, YVal_saved, score,values_TM)
         mses.append(mse_saved)
         print("mse", mse_saved)
     weights_list.append(weights_abs)
@@ -158,5 +178,6 @@ while num_cycle<cycles:
 
     countIter+=1
 
-    np.savez(output_file_name, dict_ = dict_,saved_indexes_list = saved_indexes_list, mses = mses, weights_list = weights_list, XTrain = XTrain, XTest = XTest, YTest = YTest, YTrain = YTrain, XTrainTransf_ = XTrain_transf, XTestTransf_ = XTest_transf, XTrain_ValNoCenter = XTrain_noCenter,
+    np.savez(output_folder+"/Enel_cross_val_blocks_direction_single_and_all_levels.npz", dict_ = output_dict,saved_indexes_list = saved_indexes_list, mses = mses, weights_list = weights_list,
+             XTrain = XTrain, XTest = XTest, YTest = YTest, YTrain = YTrain, XTrainTransf_ = XTrain_transf, XTestTransf_ = XTest_transf, XTrain_ValNoCenter = XTrain_noCenter,
            XValTransf_noCenter = XVal_noCenter, YTrainVal_noCenter = YTrain_noCenter, YVal_noCenter = YVal_noCenter, XTrain_Val = XTrain_, XVal = XVal_ , YVal_ = YVal_, YTrain_Val = YTrain_ )
