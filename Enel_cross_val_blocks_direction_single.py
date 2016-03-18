7c1b3baf35e7c3c2b7013b45fdcf8ed4deaf53c4
marty10 <martychuncie@alice.it>
2016-03-18 16:29:37 +0100

    correzione direzione

--- Enel_cross_val_blocks_direction_single.py
+++ Enel_cross_val_blocks_direction_single.py
@@ -53,7 +53,7 @@ XTrain_, YTrain_, X_mean, y_mean, X_std = center_data(XTrain_noCenter, YTrain_no
 XVal_, YVal_ = center_test(XVal_noCenter,YVal_noCenter,X_mean,y_mean,X_std)
 
 values_TM = []
-new_loss, _ = compute_lasso(XTrain_, YTrain_, XVal_, YVal_,score = "mean_squared_error", values_TM = np.array([[24,281], [24,214]]))
+new_loss, _ = compute_lasso(XTrain_, YTrain_, XVal_, YVal_,score = "mean_squared_error", values_TM = [])
 print("loss", new_loss)
 
 n_features_transf = XTrain_.shape[1]
